274b9ba94de564910bbc9da7aae8b81e172fe20f
mariaemmoura <mariaeduardamagalhaesmoura@gmail.com>
2024-06-13 23:47:18 -0300

    feat(p2-pdm): componente de lista com dados fakes criado

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Button from './components/Button.jsx'
+import CatList from './components/CatList.jsx'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.containerLista}>
+        <CatList />
+      </View>
       <Button></Button>
     </View>
   );
@@ -15,5 +19,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'end',
     alignItems: 'center',
+  },
+  containerLista: {
+    width: "80%",
+    height: "80%",
+    paddingTop: "10px",
+    paddingBottom: "10px"
   },
 });
